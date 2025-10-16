ae40cdc71293674ba7b343fdc99792dc93d7fda5
anaacr91 <anacascanterodriguez@gmail.com>
2025-10-16 19:28:49 +0200

    extra

--- 251013-reactApp/src/App.jsx
+++ 251013-reactApp/src/App.jsx
@@ -1,8 +1,9 @@
 import Table_Empleados from './components/Table_Empleados.jsx'
 import Navegacion from './components/Navegacion.jsx'
 import Header from './components/Header.jsx'
-import Table_Servicios from './components/Table_Servicios.jsx'
+
 import Empleado from './components/Empleado.jsx'
+import Servicio from './components/Servicio.jsx'
 
 
 
@@ -14,7 +15,7 @@ function App() {
     <Navegacion />
     <h1>Equipo</h1>
     <Table_Empleados />
-    <Table_Servicios />
+    <Servicio />
 
     </>
   )
--- 251013-reactApp/src/components/Servicio.jsx
+++ 251013-reactApp/src/components/Servicio.jsx
@@ -1,10 +1,36 @@
-function Servicio({ nombre, descripcion, precio }) {
-    return (
-        <div className="servicio">
-            <h3>{nombre}</h3>
-            <p>{descripcion}</p>
-            <p>Precio: €{precio}</p>
-        </div>
-    );
+function Servicio() {
+  const servicios = [
+    {
+      title: "Python Course",
+      price: "20€",
+      id: 1
+    },
+    {
+      title: "JavaScript Course",
+      price: "40€",
+      id: 2
+    }
+  ]
+
+  let boxes1 = []
+
+  console.log(servicios)
+  for (let i = 0; i < servicios.length; i++) {
+    console.log(servicios[i])
+    
+    boxes1.push(
+      <Servicio
+        key={servicios[i].id}
+        title={servicios[i].title}
+        price={servicios[i].price}
+        id={servicios[i].id}
+      />
+    )
+  }
+
+  return (
+    <div className="equipo">{boxes1}</div>
+  )
 }
-export default Servicio;
+
+export default Servicio
