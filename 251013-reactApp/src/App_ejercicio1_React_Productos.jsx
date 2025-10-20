e795c6b56df9876792d67c0694b99dcfa9cfcceb
anaacr91 <anacascanterodriguez@gmail.com>
2025-10-20 19:52:02 +0200

    carrito2

--- 251013-reactApp/src/App_ejercicio1_React_Productos.jsx
+++ 251013-reactApp/src/App_ejercicio1_React_Productos.jsx
@@ -2,6 +2,7 @@ import AgregarProducto from "./componentes_ej1_react/AgregarProducto"
 import ListaProductos from "./componentes_ej1_react/ListaProductos"
 import Header from "./componentes_ej1_react/Header"
 import Footer from "./componentes_ej1_react/Footer"
+import Carrito from "./componentes_ej1_react/Carrito"
 
 function App_ejercicio1_React_Productos() {  
 
